4810caf006f5ac47486eafdd99b8603d730c58c6
Brian <b.matthew.chadwick@gmail.com>
2023-10-13 17:19:02 -0400

    WIP

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
   # Remote State File Configs Start
   # Uncomment and re-run after resources are created
   backend "s3" {
-    bucket         = "terraform-20231013120105412600000001" # Update with bucket name after it is created
-    key            = "state/terraform.tfstate"
+    bucket         = "terraform-20231013205601975300000001" # Update with bucket name after it is created
+    key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform_state"
@@ -21,4 +21,29 @@ terraform {
 
 provider "aws" {
   # Configuration options
+}
+
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+  
+  cluster_name    = "my-cluster"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
 }
