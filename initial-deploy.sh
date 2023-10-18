1853af6804547a8058ef70ede37a32f1147780b9
Brian <b.matthew.chadwick@gmail.com>
2023-10-17 20:12:16 -0400

    Updated with new s3 bucket name and account ID

---
+++ initial-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# Only run this script once during initial bootstrapping
+
+# Return the aws account ID
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
+# Update the GitHub Actions workflows file
+cp resources/terraform.yml .github/workflows/
+sed -i "s/UPDATE_ME/$ACCOUNT_ID/g" .github/workflows/terraform.yml
+
+# Return remote state bucket name
+BUCKET_NAME=$(aws s3 ls | grep terraform-remote-state | cut -d " " -f 3)
+printf "BUCKET_NAME = $BUCKET_NAME\n"
+
+# Copy and add the bucket name to main.tf
+cp resources/main.tf main.tf
+sed -i "s/UPDATE_ME/$BUCKET_NAME/g" main.tf
+
+# Execute pipeline via pushing to changes to the main branch
+git add .
+git commit -m "Updated with new s3 bucket name and account ID"
+git push
+
+
+
+
+# # Initialize and apply
+# terraform init
+# # terraform apply --auto-approve
+# terraform plan
--- resources/main.tf
+++ resources/main.tf
@@ -9,7 +9,7 @@ terraform {
 
   # Remote state configs
   backend "s3" {
-    bucket         = "terraform-remote-state-1a6b61a4"
+    bucket         = "UPDATE_ME"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
