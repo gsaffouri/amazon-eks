563a5cf3ec8c9f1a7a45cf1408ff4efec2cf781e
Brian <b.matthew.chadwick@gmail.com>
2023-10-22 19:12:25 -0400

    Work In Progress...

--- main.tf
+++ main.tf
@@ -87,6 +87,7 @@ module "eks" {
     aws-efs-csi-driver = {
       most_recent                 = true
       resolve_conflicts_on_update = "PRESERVE"
+      resolve_conflicts_on_create = "PRESERVE"
       resolve_conflicts           = "NONE"
     }
     vpc-cni = {
