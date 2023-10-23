79059ccdbb28c1739400a60460e3b9e437ca70e8
Brian <b.matthew.chadwick@gmail.com>
2023-10-23 07:03:51 -0400

    New deployment

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
   # Remote state configs
   backend "s3" {
-    bucket         = "terraform-remote-state-3392cf8d"
+    bucket         = "terraform-remote-state-ed00796e"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
