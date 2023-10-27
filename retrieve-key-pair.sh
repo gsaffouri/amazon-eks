a952f3b8f08992519c5274a8124e6dd1911371d4
Brian <b.matthew.chadwick@gmail.com>
2023-10-27 17:37:10 -0400

    Deploying resources...

--- retrieve-key-pair.sh
+++ retrieve-key-pair.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-aws secretsmanager get-secret-value --secret-id eks-managed-node-key-pair | jq -r .SecretString > ~/.ssh/eks-managed-node-key-pair.sh
+aws secretsmanager get-secret-value --secret-id css-amazon-eks-key-pair | jq -r .SecretString > ~/.ssh/eks-managed-node-key-pair.sh
 
 chmod 600 ~/.ssh/eks-managed-node-key-pair.sh
