426fba62bd2c0d577d191c9dbe5f849b641334ca
Brian Chadwick <b.matthew.chadwick@gmail.com>
2025-01-28 19:38:54 -0500

    Deploying resources...

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
   # Remote state configs
   backend "s3" {
-    bucket         = "terraform-remote-state-478228a1"
+    bucket         = ""
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
