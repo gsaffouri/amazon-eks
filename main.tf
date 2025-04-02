12157f795d8077feaea64db21f010d456ccfab7e
George Saffouri <george.saffouri@outlook.com>
2025-04-02 14:57:21 -0400

    Deploying resources...

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
   # Remote state configs
   backend "s3" {
-    bucket         = "terraform-remote-state-672c42c3"
+    bucket         = "terraform-remote-state-8c1b956c"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
